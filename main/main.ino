86d43fa6d63cdaf5c0d82038d1342cb0ad2c4738
nicolas-gomez-rojas <nicogomezrojas09@gmail.com>
2023-11-19 15:53:57 -0500

    Added final code for 1st version

--- main/main.ino
+++ main/main.ino
@@ -1,38 +1,54 @@
 int white_led = 7;
 int green_led = 6;
 int red_led = 5;
-int potentiometer = A0;
+int humidity_sensor = A0;
+int light_sensor = A1;
+int water_bomb = 9;
 
+int light = 8;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(white_led, OUTPUT);
   pinMode(green_led, OUTPUT);
   pinMode(red_led, OUTPUT);
-  pinMode(potentiometer, INPUT);
+  pinMode(humidity_sensor, INPUT);
+  pinMode(light_sensor, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int potValue = analogRead(potentiometer);
-  if (potValue < 450){
+  int sensorValue = analogRead(humidity_sensor);
+  int light_val = analogRead(light_sensor);
+  if (sensorValue < 450){
+    digitalWrite(water_bomb, HIGH);
+    delay(2000);
+    digitalWrite(water_bomb, LOW);
     digitalWrite(white_led, HIGH);
     digitalWrite(green_led, LOW);
     digitalWrite(red_led, LOW);
-    Serial.print("d");
+    Serial.print("Dry");
   }
-  if (potValue >= 450 && potValue < 900){
+  if (sensorValue >= 450 && potValue < 900){
     digitalWrite(white_led, LOW);
     digitalWrite(green_led, HIGH);
     digitalWrite(red_led, LOW);
-    Serial.print("i");
+    digitalWrite(water_bomb, LOW);
+    Serial.print("Ideal");
   }
-  if (potValue > 900){
+  if (sensorValue > 900){
     digitalWrite(white_led, LOW);
     digitalWrite(green_led, LOW);
     digitalWrite(red_led, HIGH);
-    Serial.print("h");
+    digitalWrite(water_bomb, LOW);
+    Serial.print("Humid");
   }
   delay(400);
+  if (light_val < 100){
+    digitalWrite(light, HIGH);
+  }
+  if (light_val > 100){
+    digitalWrite(light, LOW);
+  }
 }
